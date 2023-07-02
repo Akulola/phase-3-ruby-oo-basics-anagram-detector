cbf1c8272bf728ff4142228a77f1865f8b82a216
Angela <angelakulola4@gmail.com>
2023-07-02 20:44:59 +0300

    COmpleted Assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word.downcase
+    end
+
+    def  match(words)
+    words.select { |w| w.downcase.chars.sort == @word.chars.sort }
+  end
+
+
+end
